9b552612c6cedc491796fce262fc2a50e19d4329
Juraj Kubričan <kubrican.juraj@gmail.com>
2018-12-08 16:52:31 +0100

    working rc missing camera positions

--- Assets/Scripts/MenuScript.cs
+++ Assets/Scripts/MenuScript.cs
@@ -1,72 +1,123 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using System.Linq.Expressions;
-using Assets.Scripts;
+﻿using TMPro;
 using UnityEngine;
 
-public class MenuScript : MonoBehaviour
+namespace Assets.Scripts
 {
-    private PlatformScript _platform;
-    private CameraScript _camera;
-
-
-    private readonly Vector3[] _positions = new Vector3[2];
-    private readonly Quaternion[] _rotations = new Quaternion[2];
-    public GameObject[] Texts;
-
-    // Use this for initialization
-    void Start()
+    public class MenuScript : MonoBehaviour
     {
-        _platform = GameObject.Find("Platform").GetComponent<PlatformScript>();
-        _camera = GameObject.Find("Main Camera").GetComponent<CameraScript>();
+        private PlatformScript _platform;
+        private CameraScript _camera;
+        public GameObject ModelHolder;
 
-        _positions[0] = new Vector3(7, 5, -2);
-        _rotations[0] = Quaternion.Euler(30, -80, 0);
-        _positions[1] = new Vector3(-2.4f, 1.9f, -4.0f);
-        _rotations[1] = Quaternion.Euler(30, -300, 0);
+        private readonly Vector3[,] _positions = new Vector3[,]
+        {
+            {new Vector3(7, 5, -2), new Vector3(-2.4f, 1.9f, -4.0f)},
+            {new Vector3(7, 5, -2), new Vector3(-2.4f, 1.9f, -4.0f)},
+            {new Vector3(7, 5, -2), new Vector3(-2.4f, 1.9f, -4.0f)},
+            {new Vector3(7, 5, -2), new Vector3(-2.4f, 1.9f, -4.0f)}
+        };
 
-        foreach (GameObject child in Texts)
+        private readonly Quaternion[,] _rotations = new Quaternion[,]
         {
-            child.SetActive(false);
-        }
-    }
+            {Quaternion.Euler(30, -80, 0), Quaternion.Euler(30, -300, 0)},
+            {Quaternion.Euler(30, -80, 0), Quaternion.Euler(30, -300, 0)},
+            {Quaternion.Euler(30, -80, 0), Quaternion.Euler(30, -300, 0)},
+            {Quaternion.Euler(30, -80, 0), Quaternion.Euler(30, -300, 0)}
+        };
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetKeyDown("r"))
+        // Use this for initialization
+        private void Start()
         {
-            toggleRotate();
-        }
-        else if (Input.GetKeyDown("1"))
-        {
-            setViewpoint(0);
-        }
-        else if (Input.GetKeyDown("2"))
-        {
-            setViewpoint(1);
-        }
-    }
-
-
-    public void toggleRotate()
-    {
-        foreach (GameObject child in Texts)
-        {
-            child.SetActive(false);
-        }
-        _camera.ResetCamera();
-        _platform.IsRotating = !_platform.IsRotating;
-    }
-
-    public void setViewpoint(int point)
-    {
-        _platform.resetPosition();
-        foreach (GameObject child in Texts)
-        {
-            child.SetActive(false);
+            _platform = GameObject.Find("Platform").GetComponent<PlatformScript>();
+            _camera = GameObject.Find("Main Camera").GetComponent<CameraScript>();
         }
 
-        _camera.GoTo(_positions[point], _rotations[point], Texts[point]);
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.GetKeyDown("r"))
+            {
+                ToggleRotate();
+            }
+            else if (Input.GetKeyDown("q"))
+            {
+                SetViewpoint(0);
+            }
+            else if (Input.GetKeyDown("w"))
+            {
+                SetViewpoint(1);
+            }
+            else if (Input.GetKeyDown("1"))
+            {
+                SetModel(0);
+                ToggleRotate(true);
+            }
+            else if (Input.GetKeyDown("2"))
+            {
+                SetModel(1);
+                ToggleRotate(true);
+            }
+            else if (Input.GetKeyDown("3"))
+            {
+                SetModel(2);
+                ToggleRotate(true);
+            }
+            else if (Input.GetKeyDown("4"))
+            {
+                SetModel(3);
+                ToggleRotate(true);
+            }
+        }
+
+
+        public void ToggleRotate()
+        {
+            ClearTexts();
+
+            _camera.ResetCamera();
+            _platform.IsRotating = !_platform.IsRotating;
+        }
+
+        public void ToggleRotate(bool rotate)
+        {
+            ClearTexts();
+
+            _camera.ResetCamera();
+            _platform.IsRotating = rotate;
+        }
+
+        public void SetViewpoint(int point)
+        {
+            _platform.resetPosition();
+            var texts = ClearTexts();
+            var i = _platform.ModelIndex;
+            _camera.GoTo(_positions[i, point], _rotations[i, point], texts[point]);
+        }
+
+
+        public void SetModel(int index)
+        {
+            _platform.SetObject(index);
+
+            print(ModelHolder.transform.GetChild(index).GetChild(1).name);
+
+            var btn1 = this.transform.GetChild(0).GetChild(1).GetChild(0).GetComponent<TextMeshProUGUI>();
+            btn1.SetText(ModelHolder.transform.GetChild(index).GetChild(1).GetChild(0).name + " (q)");
+
+            var btn2 = this.transform.GetChild(0).GetChild(2).GetChild(0).GetComponent<TextMeshProUGUI>();
+            btn2.SetText(ModelHolder.transform.GetChild(index).GetChild(1).GetChild(1).name + " (w)");
+            ToggleRotate(true);
+        }
+
+        private GameObject[] ClearTexts()
+        {
+            var texts = _platform.GetTexts();
+            foreach (var child in texts)
+            {
+                child.SetActive(false);
+            }
+
+            return texts;
+        }
     }
 }
--- Assets/Scripts/PlatformScript.cs
+++ Assets/Scripts/PlatformScript.cs
@@ -8,16 +8,21 @@ namespace Assets.Scripts
 
         private bool _isResetting = false;
 
+        public GameObject ModelHolder;
+        public int ModelIndex = 0;
+
         private Quaternion _from;
         private Quaternion _to;
         private Quaternion _initialQuaternion;
         private float _slerpDeltaTime;
+        private GameObject _spinningGameObject;
 
 
         // Use this for initialization
         void Start()
         {
             _initialQuaternion = transform.rotation;
+            SetObject(ModelIndex);
         }
 
         // Update is called once per frame
@@ -46,6 +51,24 @@ namespace Assets.Scripts
             _slerpDeltaTime = Time.time;
             _from = transform.rotation;
             _to = _initialQuaternion;
+        }
+
+        public void SetObject(int index)
+        {
+            Destroy(_spinningGameObject);
+            ModelIndex = index;
+            _spinningGameObject = Instantiate(ModelHolder.transform.GetChild(ModelIndex), transform.position,
+                Quaternion.identity).gameObject;
+            _spinningGameObject.transform.parent = transform;
+        }
+
+        public GameObject[] GetTexts()
+        {
+            return new[]
+            {
+                _spinningGameObject.transform.GetChild(1).GetChild(0).gameObject,
+                _spinningGameObject.transform.GetChild(1).GetChild(1).gameObject
+            };
         }
     }
 }
